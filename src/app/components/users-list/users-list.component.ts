731a9aa86653101067d7e3bc49ac479c1e099ca3
raifbrah <raifbrah@gmail.com>
2024-03-10 20:04:44 +0300

    Update

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserCardComponent } from '../user-card/user-card.component';
 import { UsersApiService } from '../../services/users-api.service';
 import { UsersService } from '../../services/users.service';
 import { CommonModule } from '@angular/common';
 import { User } from '../../interfaces/user';
+import {Subject, takeUntil} from 'rxjs';
 
 @Component({
   selector: 'app-users-list',
@@ -13,12 +14,13 @@ import { User } from '../../interfaces/user';
   styleUrl: './users-list.component.css'
 })
 export class UsersListComponent {
+  users: User[] = []
+  private destroy$ = new Subject<null> ();
+
   constructor(
     private usersApiService: UsersApiService,
     private usersService: UsersService,
   ) {}
-
-  users: User[] = []
 
   deleteUserCard(index: number) {
     this.usersService.deleteUser(index);
@@ -27,9 +29,15 @@ export class UsersListComponent {
   ngOnInit() {
     this.usersApiService
       .getUsers()
-      .subscribe((data: any) => {
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data: User[]) => {
         this.usersService.addUsers(data)
         this.users = this.usersService.getUsers()
       })
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(null)
+    this.destroy$.complete()
   }
 }
