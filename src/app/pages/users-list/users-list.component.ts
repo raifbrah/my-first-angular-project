d1973a1b23d67d6d4506130ed88d98442e5e3928
raifbrah <raifbrah@gmail.com>
2024-03-11 14:47:51 +0000

    Update

--- src/app/pages/users-list/users-list.component.ts
+++ src/app/pages/users-list/users-list.component.ts
@@ -4,23 +4,28 @@ import { UsersApiService } from '../../services/users-api.service';
 import { UsersService } from '../../services/users.service';
 import { CommonModule } from '@angular/common';
 import { User } from '../../interfaces/user';
-import {Subject, takeUntil} from 'rxjs';
+import { Subject, takeUntil } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-users-list',
   standalone: true,
-  imports: [UserCardComponent, CommonModule],
+  imports: [
+    UserCardComponent,
+    CommonModule,
+    MatButtonModule
+  ],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.css'
 })
 export class UsersListComponent {
   users: User[] = []
-  private destroy$ = new Subject<null> ();
+  private destroy$ = new Subject<null>();
 
   constructor(
     private usersApiService: UsersApiService,
     private usersService: UsersService,
-  ) {}
+  ) { }
 
   deleteUserCard(index: number) {
     this.usersService.deleteUser(index);
