1259c90426c7d6d9bcbbffd92ff47d016bc67a4b
raifbrah <raifbrah@gmail.com>
2024-03-26 16:51:00 +0300

    Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import {UsersListComponent} from './pages/users-list/users-list.component';
-import {HomeComponent} from './pages/home/home.component';
+import { UsersListComponent } from './pages/users-list/users-list.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
